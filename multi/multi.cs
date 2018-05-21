f2ba8d1d3044f19974e7ea8890991a89e551f62a
pH200 <ph.minamo@cytisan.com>
2018-05-21 20:01:49 +0700

    files

---
+++ multi/multi.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace multi
+{
+    public class Class20
+    {
+    }
+}
---
+++ net462/net462.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using multi;
+
+namespace net462
+{
+    public class Class1
+    {
+        public void Foo()
+        {
+            new Class20();
+        }
+    }
+}
